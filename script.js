89db0c56e72c2de26af7e6d3d16c3fcfa8196f41
@amritmohan <amritmohan2284@gmail.com>
2025-02-06 13:15:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	 // for (let i = 0; i < str.length; i++) {
+  //       let char = str[i];
+  //       if (str.indexOf(char) === str.lastIndexOf(char)) {
+  //           return char;
+  //       }
+  //   }
+  //   return null;
+	for (let char of str) {
+        if (str.indexOf(char) === str.lastIndexOf(char)) {
+            return char;
+        }
+    }
+    return null;
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
